3795c448e88a3ad0b5ee036167fd1c5689519f8a
markaxelus <mrkaxelus@gmail.com>
2026-01-29 01:54:23 -0800

    feat: added 10000 mock fire markers

--- app/app/components/Map.tsx
+++ app/app/components/Map.tsx
@@ -1,20 +1,40 @@
 'use client'
+import { useMemo } from 'react';
 import Map, { Marker } from 'react-map-gl/maplibre';
 import 'maplibre-gl/dist/maplibre-gl.css';
+import FPSCounter from './FPSCounter';
 
 export default function MapComponent() {
+
+  const points = useMemo(() => {
+    return Array.from({ length: 10000 }).map((_, i) => ({
+      id: i,
+      longitude: -123.37 + (Math.random() - 0.5) * 5,
+      latitude: 48.4 + (Math.random() - 0.5) * 5,
+    }));
+  }, []);
+
   return (
-    <Map
-      initialViewState={{
-        longitude: -122.4,
-        latitude: 37.8,
-        zoom: 5
-      }}
-      style={{ width: '100%', height: '1279px' }}
-      mapStyle="https://demotiles.maplibre.org/style.json"
-    >
-      <Marker longitude={-122.4} latitude={37.8} color="red" />
-    </Map>
+    <div style={{ width: '100%', height: '100vh', position: 'relative' }}>
+      <FPSCounter />
+      <Map
+        initialViewState={{
+          longitude: -123.37,
+          latitude: 48.4,
+          zoom: 8
+        }}
+        mapStyle="https://demotiles.maplibre.org/style.json"
+      >
+        {points.map(point => (
+          <Marker 
+            key={point.id} 
+            longitude={point.longitude} 
+            latitude={point.latitude} 
+            color="red" 
+          />
+        ))}
+      </Map>
+    </div>
   );
 }
 
