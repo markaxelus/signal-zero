bb9fbcd7c4c398f102920fd6ad85d79ccee74044
markaxelus <mrkaxelus@gmail.com>
2026-01-29 02:08:03 -0800

    dep: mapboxgl

--- app/app/components/Map.tsx
+++ app/app/components/Map.tsx
@@ -2,21 +2,11 @@
 import { useMemo } from 'react';
 import Map, { Marker } from 'react-map-gl/maplibre';
 import 'maplibre-gl/dist/maplibre-gl.css';
-import FPSCounter from './FPSCounter';
 
 export default function MapComponent() {
 
-  const points = useMemo(() => {
-    return Array.from({ length: 10000 }).map((_, i) => ({
-      id: i,
-      longitude: -123.37 + (Math.random() - 0.5) * 5,
-      latitude: 48.4 + (Math.random() - 0.5) * 5,
-    }));
-  }, []);
-
   return (
     <div style={{ width: '100%', height: '100vh', position: 'relative' }}>
-      <FPSCounter />
       <Map
         initialViewState={{
           longitude: -123.37,
@@ -25,14 +15,11 @@ export default function MapComponent() {
         }}
         mapStyle="https://demotiles.maplibre.org/style.json"
       >
-        {points.map(point => (
-          <Marker 
-            key={point.id} 
-            longitude={point.longitude} 
-            latitude={point.latitude} 
+          <Marker
+            longitude={ -123.37} 
+            latitude={48.4} 
             color="red" 
           />
-        ))}
       </Map>
     </div>
   );
